5e38e1f54bf3ed24d36390edd6d131d6fe1620d5
Artem <84618666+TemaI1@users.noreply.github.com>
2024-09-10 09:34:51 +0300

    Update getIBlockID.php

--- function/getIBlockID.php
+++ function/getIBlockID.php
@@ -12,14 +12,3 @@ function GetIBlockID($code){
     ]) -> fetch();
     return $iBlock["ID"];
 }
-
-//Получение объекта сущности highloadblock по его названию таблицы в БД (b_hlbd)
-use Bitrix\Main\Loader; 
-Loader::includeModule("highloadblock"); //подключение модуля
-function getHBbyName($code){
-    $hlblock = Bitrix\Highloadblock\HighloadBlockTable::getList([
-        'filter' => ['=TABLE_NAME' => $code]
-    ])->fetch();
-    $entity_data_class = (Bitrix\Highloadblock\HighloadBlockTable::compileEntity($hlblock))->getDataClass();
-    return $entity_data_class;
-}
