2dcfd3b0ed978b449a2756b13e465b0afa28128d
tema may <xTema05x@mail.ru>
2025-09-30 16:55:52 +0300

    update file

--- Bitrix Developer Course/Основы PHP/Основы работы с объектами/main.php
+++ Bitrix Developer Course/Основы PHP/Основы работы с объектами/main.php
@@ -1,10 +1,10 @@
 <?php
 
 // номер года и месяца, с которых начинать расчёт
-$yearMonthNum = [2025, 4];
+$yearMonthNum = [2025, 10];
 
 // количество месяцев, для которых производить расчёт
-$numberMonths = 1;
+$numberMonths = 5;
 
 function getWorkSchedule($yearMonthNum, $numberMonths){
 
@@ -12,37 +12,37 @@ function getWorkSchedule($yearMonthNum, $numberMonths){
         $date = DateTime::createFromFormat('!m', $yearMonthNum[1]);
         $monthName = $date->format('F'); 
         echo "\n" . "Название месяца: " . $monthName;
-        if ($yearMonthNum[1] >= 12) {
-            $yearMonthNum[1] = 1;
-        } else {
-            $yearMonthNum[1]++;
-        }
 
         $days_in_month = cal_days_in_month(CAL_GREGORIAN, $yearMonthNum[1], $yearMonthNum[0]);
 
         echo "\n" . "Список всех дней месяца " . $monthName . ": ";
 
+        $nonWorkingDays = 2;
+
         for ($day = 1; $day <= $days_in_month; $day++) {
             $date = new DateTime();
             $date->setDate($yearMonthNum[0], $yearMonthNum[1], $day);
 
-            // Получаем номер дня недели
-            $dayOfWeek = ($day - 1) % 3;
-
             $weekendDay= (int)$date->format('N');
-
             if ($weekendDay >= 6) {
-                echo " - \033[33m$day\033[0m"; // желтый
-                continue; // пропускаем текущий день
-            }
-
-            // Определяем, рабочий или выходной
-            if ($dayOfWeek == 0) {
-                echo " - \033[32m$day\033[0m"; // зеленый
+                $nonWorkingDays++;
+                echo " - \033[33m$day\033[0m"; // желтый - сб, вс
+            } else if ($nonWorkingDays < 2){
+                $nonWorkingDays++;
+                echo " - \033[31m$day\033[0m"; // красный - выходной день
             } else {
-                echo " - \033[31m$day\033[0m"; // красный
+                $nonWorkingDays = 0;
+                echo " - \033[32m$day\033[0m"; // зеленый - рабочий день
             }
         }
+
+        if ($yearMonthNum[1] >= 12) {
+            $yearMonthNum[1] = 1;
+            $yearMonthNum[0]++;
+        } else {
+            $yearMonthNum[1]++;
+        }
+
         echo "\n";
     }
 
